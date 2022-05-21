ecd7afa2b86d2e3983c2ba74c27f67ee6e05cf8f
Natalia <Deva2014@yandex.ru>
2022-05-21 16:04:00 +0300

    Задача 19

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Введите пятизначиное число: ");
+//int num = new Random().Next(1, 100);
+int num = int.Parse(Console.ReadLine());
+int r, sum=0, t;
+for (t=num; num!=0; num = num/10)
+{
+r = num % 10;
+sum=sum*10+r;
+}
+if (t==sum)
+{
+    Console.Write($"{t}  палиндром" , t);
+}
+    else
+    {
+        Console.Write($"{t} не палинром", t);
+    }
+
