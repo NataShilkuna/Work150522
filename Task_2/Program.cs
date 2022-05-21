bdf78beda6b2a72db9e7a3515389ce90ecd369c4
Natalia <Deva2014@yandex.ru>
2022-05-21 15:11:07 +0300

    Задача 23

---
+++ Task_2/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("|{0,10}|{1,10}", "Число", "Корень");
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine("|{0,10}|{1,10}|", i, Math.Pow(i, 3));
+}
